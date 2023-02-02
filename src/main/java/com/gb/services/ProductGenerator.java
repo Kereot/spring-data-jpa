55c2f93f4b50f5d11b69fdf0024b4116a265e700
Kereot <eepdmitriy@gmail.com>
2023-02-02 18:54:10 +0300

    HW15

--- src/main/java/com/gb/services/ProductGenerator.java
+++ src/main/java/com/gb/services/ProductGenerator.java
@@ -1,24 +1,21 @@
 package com.gb.services;
 
+import com.gb.aspect.Timer;
 import com.gb.entities.Product;
-import com.gb.entities.User;
 import com.gb.repositories.ProductRepository;
-import com.gb.repositories.UserRepository;
 import com.github.javafaker.Faker;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Timer
 @Component
+@RequiredArgsConstructor
 public class ProductGenerator {
     private static final Faker faker = new Faker();
 
-    @Autowired
-    private ProductRepository productRepository;
-//    @Autowired
-//    private UserRepository userRepository;
-//    Was used for testing BD
+    private final ProductRepository productRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     public void generateProductsOnStartup() {
@@ -26,16 +23,6 @@ public class ProductGenerator {
             productRepository.save(generate());
         }
     }
-//    Was used for testing BD
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void fillDamnDatabase() {
-//        User user = new User();
-//        user.setUsername("user1");
-//        user.setPassword("$2a$10$9Feu10NvX5ee9.LIzEWyMOWbShX9ScOLxdBwqnSnjLKRtPO.adR5e");
-//        user.setEmail("user1@gmail.com");
-//        userRepository.save(user);
-//    }
-
     public static Product generate() {
         Product product = new Product();
         product.setName(faker.beer().name());
